80d723827a5e34dd953a1a1709651064b1637d10
Kaya Celebi <kayacelebi17@gmail.com>
2020-10-31 19:31:16 -0400

    changing run

--- run.sh
+++ run.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
-	echo "Please provide a test input of the form:"
-	echo "				./run.sh <TEST NAME>"
-elif [ $1 = "simple" ]
-then
-	M1="tests/inputs/simple0/bin/m0.bin"
-	M2="tests/inputs/simple0/bin/m1.bin"
-	INPUT="tests/inputs/simple0/bin/inputs/input0.bin"
-	OUTPUT="tests/outputs/test_basic_main/student_basic_output.bin"
+echo "Running your example: " $1
 
-	java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
 
-	python3 tools/convert.py --to-ascii $OUTPUT "output.txt"
-elif [ $1 == "mnist" ]
-then
-	echo "The mnist test"	
-fi 
+echo "This should output the number: " 
 
-echo "Done"
+cat "test/inputs/txt/labels/label"$1".txt"
+
+M1="tests/inputs/bin/m0.bin"
+M2="tests/inputs/bin/m1.bin"
+
+INPUT="tests/inputs/bin/inputs/mnist_input"$1".bin"
+OUTPUT="tests/outputs/user_output.bin"
+
+echo "Running network:"
+
+java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
+
