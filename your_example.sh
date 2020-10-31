6ae79de78460d1a1269a790b616f7d8430d93b77
Kaya Celebi <kayacelebi17@gmail.com>
2020-10-31 19:08:15 -0400

    testing consistency

--- your_example.sh
+++ your_example.sh
@@ -2,15 +2,15 @@
 
 echo "Running your example: " $1
 
-python3 tests/inputs/mnist/bmp_to_bin.py $1
+python3 tests/inputs/bmp_to_bin.py $1
 
 echo "Created binary file"
 
-M1="tests/inputs/mnist/bin/m0.bin"
-M2="tests/inputs/mnist/bin/m1.bin"
+M1="tests/inputs/bin/m0.bin"
+M2="tests/inputs/bin/m1.bin"
 #INPUT="tests/inputs/mnist/bin/inputs/mnist_input0.bin"
 INPUT=$1".bin"
-OUTPUT="tests/outputs/test_mnist_main/user_output.bin"
+OUTPUT="tests/outputs/user_output.bin"
 
 echo $INPUT
 java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
