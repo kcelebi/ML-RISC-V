062e004ae30ff565a925b2943e6c5c3cd55a30df
Kaya Celebi <kayacelebi17@gmail.com>
2020-10-31 19:37:43 -0400

    fixed .sh files

--- run.sh
+++ run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "Running your example: " $1
+if [ ! -e "tests/inputs/txt/labels/label"$1".txt" ]
+then
+	echo "Please put in a number between 0 and 8"
+else
+	echo "Running your example: " $1
 
+	echo "This should output the number: " 
 
-echo "This should output the number: " 
+	cat "tests/inputs/txt/labels/label"$1".txt"
 
-cat "test/inputs/txt/labels/label"$1".txt"
+	M1="tests/inputs/bin/m0.bin"
+	M2="tests/inputs/bin/m1.bin"
 
-M1="tests/inputs/bin/m0.bin"
-M2="tests/inputs/bin/m1.bin"
+	INPUT="tests/inputs/bin/inputs/mnist_input"$1".bin"
+	OUTPUT="tests/outputs/user_output.bin"
 
-INPUT="tests/inputs/bin/inputs/mnist_input"$1".bin"
-OUTPUT="tests/outputs/user_output.bin"
+	echo "Running network:"
 
-echo "Running network:"
+	java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
 
-java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
-
+fi
--- your_example.sh
+++ your_example.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo "Running your example: " $1
+if [ ! -e  $1".bmp" ]
+then 
+	echo "Please input a valid file"
+else
+	echo "Running your example: " $1
 
-python3 tests/inputs/bmp_to_bin.py $1
+	python3 tests/inputs/bmp_to_bin.py $1
 
-echo "Created binary file"
+	echo "Created binary file"
 
-M1="tests/inputs/bin/m0.bin"
-M2="tests/inputs/bin/m1.bin"
-#INPUT="tests/inputs/mnist/bin/inputs/mnist_input0.bin"
-INPUT=$1".bin"
-OUTPUT="tests/outputs/user_output.bin"
+	M1="tests/inputs/bin/m0.bin"
+	M2="tests/inputs/bin/m1.bin"
+	#INPUT="tests/inputs/mnist/bin/inputs/mnist_input0.bin"
+	INPUT=$1".bin"
+	OUTPUT="tests/outputs/user_output.bin"
 
-echo $INPUT
-java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
-
+	echo $INPUT
+	java -jar tools/venus.jar src/main.s -ms -1 $M1 $M2 $INPUT $OUTPUT
+fi
